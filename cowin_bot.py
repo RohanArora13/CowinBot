99f2e2f6ddd46a404f90d3a93632c02402bd9e21
Rohan Arora <RohanArora1313@gmail.com>
2021-11-17 18:18:26 +0530

    removed emails

--- cowin_bot.py
+++ cowin_bot.py
@@ -152,7 +152,7 @@ def send_mail(one_center,checkDate):
     fromaddr = "rsachannel11@gmail.com"
 
     #people to send
-    toaddr = ["prathameshvhanmane@gmail.com","adityakashyap937@gmail.com","workdivij@gmail.com","rohanarora1313@gmail.com","rihit555@digdig.org","amitarora399@gmail.com","karandikarshreyash@gmail.com","Barcelonapratik@gmail.com","saurabhvirola@gmail.com",""]
+    toaddr = ["rihit555@digdig.org"]
 
     #dropped_df=final_df.drop(['State'], axis=1)
 
